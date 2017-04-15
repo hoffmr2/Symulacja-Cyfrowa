1f5fc29fd201290aca23f7769ec2bd6897f19223
hoffmr2 <hoffmr2@wp.pl>
2017-04-15 16:53:14 +0200

    receiver class i generatory liczb losowych

---
+++ WirelessNetworkSymulation/WirelessNetworkComponents/SimulationRandomGenerators.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RandomGenerators;
+
+namespace WirelessNetworkComponents
+{
+   
+    public struct SimulationRandomGenerators
+    {
+        public UniformRandomGenerator TransmissionTime;//CTPk
+        public UniformRandomGenerator BackofftimerValue; //BT
+        public ExponentialRandomGenerator GenerationTime; //CGPk
+    }
+}
